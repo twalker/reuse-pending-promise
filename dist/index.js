d4f79c87881779058c5e8a2add647bb8f39ca1bf
tim walker <tim.twalker@gmail.com>
2020-07-25 08:33:59 -0700

    Update dependencies

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-module.exports = (fn, getKey) => (...args) => {
-  const cacheKey = typeof getKey === 'function' ? getKey(...args) : args[0] || 'noargs';
-  fn.pending = fn.pending || new Map();
-  if (!fn.pending.has(cacheKey)) {
-    fn.pending.set(cacheKey,
-      fn(...args)
-          .then(value => {
-            fn.pending.delete(cacheKey);
-            return value
-        })
-          .catch(error => {
-            fn.pending.delete(cacheKey);
-            throw error
-        })
-    );
-  }
-  return fn.pending.get(cacheKey)
+module.exports = function (fn, getKey) {
+  return function () {
+    var cacheKey = typeof getKey === 'function' ? getKey.apply(void 0, arguments) : (arguments.length <= 0 ? undefined : arguments[0]) || 'noargs';
+    fn.pending = fn.pending || new Map();
+
+    if (!fn.pending.has(cacheKey)) {
+      fn.pending.set(cacheKey, fn.apply(void 0, arguments).then(function (value) {
+        fn.pending["delete"](cacheKey);
+        return value;
+      })["catch"](function (error) {
+        fn.pending["delete"](cacheKey);
+        throw error;
+      }));
+    }
+
+    return fn.pending.get(cacheKey);
+  };
 };
