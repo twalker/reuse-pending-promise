cc8002f89cbfa93c553385a3b9e7e16f1b318914
tim walker <tim.twalker@gmail.com>
2020-07-26 10:51:58 -0700

    0.3.0

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,66 @@
 'use strict';
 
-var reusePendingPromise = function reusePendingPromise(fn, getKey) {
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
+var defaultOptions = {
+  getCacheKey: function getCacheKey() {
+    return (arguments.length <= 0 ? undefined : arguments[0]) || 'noargs';
+  }
+};
+
+var reusePendingPromise = function reusePendingPromise(fn) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
   return function () {
-    var cacheKey = typeof getKey === 'function' ? getKey.apply(void 0, arguments) : (arguments.length <= 0 ? undefined : arguments[0]) || 'noargs';
+    var options_ = _objectSpread2(_objectSpread2({}, defaultOptions), options);
+
+    var cacheKey = options_.getCacheKey.apply(options_, arguments);
     fn.pending = fn.pending || new Map();
 
     if (!fn.pending.has(cacheKey)) {
