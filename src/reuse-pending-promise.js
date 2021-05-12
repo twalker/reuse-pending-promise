b20f2148215462632cfd6c06b8ef85e734c15c67
tim walker <tim.twalker@gmail.com>
2021-05-11 19:32:24 -0700

    Update dev dependencies (#19)
    * Update dev dependencies
    
    * drop node 10 support
    
    * Delint
    
    * less specific lint-action version

--- src/reuse-pending-promise.js
+++ src/reuse-pending-promise.js
@@ -2,17 +2,19 @@ const defaultOptions = {
   getCacheKey: (...args) => args[0] || 'noargs'
 }
 
-export const reusePendingPromise = (fn, userOptions = {}) => (...args) => {
-  const options = {...defaultOptions, ...userOptions}
-  const cacheKey = options.getCacheKey(...args)
+export const reusePendingPromise =
+  (fn, userOptions = {}) =>
+  (...args) => {
+    const options = {...defaultOptions, ...userOptions}
+    const cacheKey = options.getCacheKey(...args)
 
-  fn.pending = fn.pending || new Map()
-  if (!fn.pending.has(cacheKey)) {
-    fn.pending.set(
-      cacheKey,
-      fn(...args).finally(() => fn.pending.delete(cacheKey))
-    )
+    fn.pending = fn.pending || new Map()
+    if (!fn.pending.has(cacheKey)) {
+      fn.pending.set(
+        cacheKey,
+        fn(...args).finally(() => fn.pending.delete(cacheKey))
+      )
+    }
+
+    return fn.pending.get(cacheKey)
   }
-
-  return fn.pending.get(cacheKey)
-}
